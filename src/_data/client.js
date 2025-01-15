8430c528a5b2644adb339b15e9477dcf1adb81a2
weblumee <weblumee@gmail.com>
2025-01-14 19:32:14 -0600

    new changes

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,20 +1,20 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "Weblumee",
+    "email": "info@weblumee.com",
+    "phoneForTel": "331-707-8605",
+    "phoneFormatted": "(331) 707-8605",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
+        "lineOne": "test",
+        "lineTwo": "",
+        "city": "",
+        "state": "",
+        "zip": "",
+        "mapLink": ""
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
         "instagram": "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://www.weblumee.com"
 };
